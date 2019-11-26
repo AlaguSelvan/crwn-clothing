279c3864fbeecbdff15d93770a024d4ab46060af
AlaguSelvan <alaguselvan97@gmail.com>
2019-11-26 22:53:16 +0530

    css setup well structured added header added shop page and router setup

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './menu-item.styles.scss'
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => (
   <div className={`${size} menu-item`}>
     <div className='background-image'
       style={{
